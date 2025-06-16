10938ff3650692b576a74c7ca4a3cc9627146785
bjmnh <88810487+bjmnh@users.noreply.github.com>
2025-06-16 06:43:37 -0700

    Fix Vite build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: path.resolve(__dirname, './'),
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
+  }
 });

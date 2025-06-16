3ccca4dd39e9a9d1d0781a99c460fc5857f638ce
bjmnh <88810487+bjmnh@users.noreply.github.com>
2025-06-16 06:41:34 -0700

    Fix Vite config root path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.',
+  root: path.resolve(__dirname, './'),
   plugins: [react()],
   resolve: {
     alias: {
